a449ff35b8435e655330b59e4067fde195ebb8be
chen-charles <chen-charles@users.noreply.github.com>
2022-12-03 00:57:07 -0800

    tests: fix cleanup re-runs
    Signed-off-by: chen-charles <chen-charles@users.noreply.github.com>

--- tests/cleanup2.c
+++ tests/cleanup2.c
@@ -145,6 +145,8 @@ int pthread_test_cleanup2()
   int i;
   pthread_t t[NUMTHREADS + 1];
 
+  pop_count.i = 0;
+  pop_count.null = 0;
   pte_osMutexCreate(&pop_count.cs);
 
   assert((t[0] = pthread_self()) != NULL);
